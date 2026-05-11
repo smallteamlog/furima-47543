f1dbf942c3a7dd98027aeb21706a37085f749554
smallteamlog <smallteamlog@gmail.com>
2026-05-11 13:46:17 +0900

    テスト項目の追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe Item, type: :model do
       end
     end
     context '新規登録できない場合' do
+      it 'userが紐付いていなければ登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'User must exist'
+      end
       it '商品画像が空では登録できない' do
         @item.image = nil
         @item.valid?
